e58bccc9ae685519f2f47c228f5eb6e1a81d0fea
PluT00 <314lut00@gmail.com>
2022-12-07 20:14:04 +0300

    Rewrite Ctrl-w with <leader>w

--- core/keybindings.lua
+++ core/keybindings.lua
@@ -8,6 +8,7 @@ keymap.set('n', '<ESC>', ':noh<CR>')    -- Disable search results highlight.
 keymap.set({'n', 'x'}, 'cp', '"+y')     -- Copy to clipboard.
 keymap.set({'n', 'x'}, 'cv', '"+p')     -- Paste from clipboard.
 keymap.set({'n', 'x'}, 'x', '"_x')      -- Override 'x' to delete text without buffering
+keymap.set({'n', 'x'}, '<leader>w', '<C-w>')
 
 keymap.set('n', '<leader>ft', ':NERDTreeToggle<CR>')
 keymap.set('n', '<leader>ff', ':NERDTreeRefreshRoot<CR>:NERDTreeFocus<CR>')

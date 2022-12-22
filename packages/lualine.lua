543929ec57b7ae57f67f1e362dbcab0c0d811f1e
PluT00 <314lut00@gmail.com>
2022-12-22 22:44:43 +0300

    Enable icons from NerdFonts in lualine plugin

--- packages/lualine.lua
+++ packages/lualine.lua
@@ -3,12 +3,6 @@ require'lualine'.setup{
   options = {
     icons_enabled = true,
     theme = 'gruvbox-material',
-    component_separators = { left = '|', right = '|'},
-    section_separators = { left = '|', right = '|'},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
     ignore_focus = {},
     always_divide_middle = true,
     globalstatus = false,
@@ -29,7 +23,7 @@ require'lualine'.setup{
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}

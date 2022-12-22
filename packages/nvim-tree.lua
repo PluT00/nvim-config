a9cee12a12770809ad79d5f30e2550baa32f3992
PluT00 <314lut00@gmail.com>
2022-12-22 23:02:10 +0300

    Change nvim-tree mappings (vsplit, split, tabnew)

--- packages/nvim-tree.lua
+++ packages/nvim-tree.lua
@@ -1,6 +1,14 @@
 require("nvim-tree").setup({
   view = {
     adaptive_size = true,
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = "v", action = "vsplit" },
+        { key = "s", action = "split" },
+        { key = "t", action = "tabnew" },
+      }
+    }
   },
   renderer = {
     group_empty = true,

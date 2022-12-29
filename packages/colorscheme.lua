c9da5653b9254c8075365a1757d48b6b239a8c0e
PluT00 <314lut00@gmail.com>
2022-12-29 12:14:30 +0300

    Switch back to 'gruvbox-material'

--- packages/colorscheme.lua
+++ packages/colorscheme.lua
@@ -1,15 +1,15 @@
 -- Enable 'gruvbox-material' colorscheme.
--- vim.cmd('colorscheme gruvbox-material')
+vim.cmd('colorscheme gruvbox-material')
 
 -- Default 'nord' colorscheme settings.
-vim.g.nord_contrast = false
-vim.g.nord_borders = false
-vim.g.nord_disable_background = false
-vim.g.nord_cursorline_transparent = false
-vim.g.enable_sidebar_background = false
-vim.g.nord_italic = true
-vim.g.nord_uniform_diff_background = false
-vim.g.nord_bold = true
+-- vim.g.nord_contrast = false
+-- vim.g.nord_borders = false
+-- vim.g.nord_disable_background = false
+-- vim.g.nord_cursorline_transparent = false
+-- vim.g.enable_sidebar_background = false
+-- vim.g.nord_italic = true
+-- vim.g.nord_uniform_diff_background = false
+-- vim.g.nord_bold = true
 
 -- Enable 'nord' colorscheme.
-require('nord').set()
+-- require('nord').set()
